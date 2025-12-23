753c722a82179f8c222ea899237cf36e956ebe86
objktt <objktt@gmail.com>
2025-12-23 22:03:20 +0900

    Update App Store download link

--- components/sections/CTA.tsx
+++ components/sections/CTA.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 import { useLanguage } from "@/context/LanguageContext";
 
@@ -45,12 +46,12 @@ export default function CTA() {
           </h2>
           
           <div className="flex flex-wrap items-center justify-center gap-4">
-            <button 
-              type="button" 
-              onClick={() => handleButtonClick('appstore')}
+            <Link 
+              href="https://apps.apple.com/kr/app/kolektt/id6743063796"
+              target="_blank"
               className="group/btn relative flex items-center px-6 py-3 rounded-xl border-none min-w-[200px] hover:scale-105 active:scale-95 cursor-pointer shadow-lg transition-all overflow-hidden bg-white text-black hover:bg-gray-200"
             >
-              <div className={`flex items-center transition-opacity duration-300 w-full ${(activeButton === 'appstore') ? 'opacity-20' : 'group-hover/btn:opacity-20'}`}>
+              <div className="flex items-center w-full">
                 <svg xmlns="http://www.w3.org/2000/svg" width="28px" fill="#000" className="mr-3" viewBox="0 0 22.773 22.773">
                   <path d="M15.769 0h.162c.13 1.606-.483 2.806-1.228 3.675-.731.863-1.732 1.7-3.351 1.573-.108-1.583.506-2.694 1.25-3.561C13.292.879 14.557.16 15.769 0zm4.901 16.716v.045c-.455 1.378-1.104 2.559-1.896 3.655-.723.995-1.609 2.334-3.191 2.334-1.367 0-2.275-.879-3.676-.903-1.482-.024-2.297.735-3.652.926h-.462c-.995-.144-1.798-.932-2.383-1.642-1.725-2.098-3.058-4.808-3.306-8.276v-1.019c.105-2.482 1.311-4.5 2.914-5.478.846-.52 2.009-.963 3.304-.765.555.086 1.122.276 1.619.464.471.181 1.06.502 1.618.485.378-.011.754-.208 1.135-.347 1.116-.403 2.21-.865 3.652-.648 1.733.262 2.963 1.032 3.723 2.22-1.466.933-2.625 2.339-2.427 4.74.176 2.181 1.444 3.457 3.028 4.209z" />
                 </svg>
@@ -59,12 +60,7 @@ export default function CTA() {
                   <span className="text-lg font-bold">App Store</span>
                 </div>
               </div>
-              <div className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${(activeButton === 'appstore') ? 'opacity-100' : 'opacity-0 group-hover/btn:opacity-100'}`}>
-                <span className="text-base font-bold font-korean">
-                  {language === "KO" ? "준비중" : "Coming Soon"}
-                </span>
-              </div>
-            </button>
+            </Link>
 
             <button 
               type="button" 
